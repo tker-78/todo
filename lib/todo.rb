b77b0ddb9ade747999f82c92da1a9139d0f03386
takuya kinoshita <kktak02@gmail.com>
2022-05-02 06:40:26 +0900

    add execute to command

--- lib/todo.rb
+++ lib/todo.rb
@@ -3,6 +3,8 @@
 require_relative "todo/version"
 require_relative "todo/command"
 require_relative "todo/db"
+require_relative "todo/task"
+
 module Todo
   class Error < StandardError; end
   # Your code goes here...
--- lib/todo/command.rb
+++ lib/todo/command.rb
@@ -1,5 +1,11 @@
 module Todo
   class Command
+    def execute
+      DB.prepare
+    end
 
+    def create_task(name, content)
+      Task.create!(name: name, content: content).reload
+    end
   end
 end
